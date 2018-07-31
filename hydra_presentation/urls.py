8175d25b0df8403690edc029a50ff593baa5ea00
László Hegedűs <heglas11@gmail.com>
2018-08-01 00:26:22 +0200

    Fix static resources.

--- hydra_presentation/urls.py
+++ hydra_presentation/urls.py
@@ -13,26 +13,32 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import os
 from django.conf import settings
 from django.conf.urls import url
 from django.urls import path
-from django.views.generic import TemplateView, RedirectView
 from django.views.static import serve
+
 from . import views
 
 urlpatterns = [
+    # Static service worker from static files
     url(r'^service-worker.js$', serve, kwargs={
         'path': 'service-worker.js',
         'document_root': settings.STATIC_ROOT
     }, name='service_worker'),
+    # Dynamic Polymer application view
     url(r'^src/my-app.html', views.application, name='application'),
+    # Dynamic Polymer page view
     path(r'page/my-<str:path>.html', views.page, name='page'),
+    # Static Polymer application source files
     url(r'^src/(?P<path>.*)$', serve, kwargs={
         'document_root': settings.STATIC_ROOT + '/src'
     }, name='source'),
+    # Dynamic Polymer application shell view
     url(r'^index.html', views.index, name='index'),
+    # API: User info
     url(r'^user-info/$', views.user_info, name='user_info'),
+    # API: Application entry point list
     url(r'^applications/$', views.application_index, name='application_index'),
 
     # url(r'^(?P<path>)/(?P<path>.html)$', views.page, name='bower'),
